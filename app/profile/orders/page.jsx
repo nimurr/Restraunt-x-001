acb993fc009e3d1f883091343865bee749fdfc5f
Dipu Debnath <dipudebnath966@gmail.com>
2023-12-07 16:35:08 +0600

    user profile

--- app/profile/orders/page.jsx
+++ app/profile/orders/page.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
 
-const page = () => {
+const page =() => {
     const orders = [
         { id: 1, name: 'Barger', img: 'https://i.ibb.co/xCJCRYS/chicken-with-sambal-sauce-deep-fried-hot-spicy-menu-indonesia-combination-malaysian-fusion-style-sid.jpg', quatity: 2, price: 40 },
         { id: 2, name: 'Barger', img: 'https://i.ibb.co/FJNqb44/images.jpg', quatity: 2, price: 40 },
@@ -9,6 +9,9 @@ const page = () => {
         { id: 4, name: 'Barger', img: 'https://i.ibb.co/g6jCxmr/download.jpg', quatity: 2, price: 40 },
 
     ]
+    const totalPrice = orders.reduce((accumulator, order) => accumulator + order.price, 0);
+
+
     return (
         <div>
             <div>
@@ -35,6 +38,13 @@ const page = () => {
                                     <td className="font-semibold px-4 border-b">${item.price.toFixed(2)}</td>
                                 </tr>
                             ))}
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <th>Total </th>
+                                <td className='font-bold'>={totalPrice}</td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
